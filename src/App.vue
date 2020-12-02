28b8ad63cfc21a88b76053ce902e6a270ae83791
David <milosevicdavid@gmail.com>
2020-12-02 23:58:33 +0100

    Extended version

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <div id="container">
-    <v-select
-      class="style-chooser"
-      v-model="query"
-      label="title"
-      :clearable="false"
-      :options="options"
-      placeholder="Please start entering text to search"
-    >
-    </v-select>
-    <div>
-      {{ query.title }} <br />
-      {{ query.body }}
+  <div class="container">
+    <div class="wrapper">
+      <v-select
+        class="style-chooser"
+        v-model="query"
+        label="title"
+        :clearable="false"
+        :options="options"
+        placeholder="Please start entering text to search"
+      >
+        <template v-slot:option="option">
+          <div class="title">
+            {{ option.title }}
+          </div>
+          <div class="subtitle">
+            {{ `${option.body.slice(0, 40)} ...` }}
+          </div>
+        </template>
+        <template v-slot:no-options="{ search, searching }">
+          <template v-if="searching">
+            <span class="no-search">
+              Sorry, we couldn't find anything :(
+            </span>
+          </template>
+        </template>
+      </v-select>
+      <div v-if="query" class="card-wrapper">
+        <div class="card">
+          <span class="card-title"
+            ><h1>{{ query.title }}</h1></span
+          >
+          <span class="card-body">{{ `${query.body}.` }}</span>
+        </div>   
+      </div>
+      <div class="post-holder" v-else>... your selected post will display here</div>
     </div>
   </div>
 </template>
@@ -38,13 +60,42 @@ export default {
 </script>
 
 <style lang="scss">
+
+body {
+  background-color: rgb(171, 251, 146, 0.4);
+
+}
+
+.container {
+  margin-top: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.wrapper {
+  width: 60vw;
+}
+
+.post-holder{
+  color: #69c28e;
+  text-align: center;
+  margin-top: 100px;
+  font-size: 24px;
+}
+
 .style-chooser {
-  .vs__search::placeholder,
+  
   .vs__dropdown-toggle {
     color: #394066;
     font-size: 14px;
     height: 44px;
     border-radius: 8px;
+  }
+
+  .vs__search::placeholder {
+    opacity: 0.5;
   }
 
   .vs__dropdown-menu {
@@ -66,6 +117,60 @@ export default {
   .vs__clear,
   .vs__open-indicator {
     fill: #394066;
+  }
+}
+
+.subtitle {
+  font-size: 11px;
+  display: block;
+  color: #299553;
+}
+
+.no-search {
+  color: #dedede;
+}
+
+.title {
+  font-weight: 600;
+  margin-top: 10px;
+}
+
+.subtitle {
+  font-weight: 600;
+  border-bottom: 1px solid #69c28e;
+}
+
+.card-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  width: 50vw;
+  margin-top: 50px;
+
+  &-title {
+    display: block;
+    text-transform: capitalize;
+    background-color: #69c28e;
+    padding: 20px;
+    color: #fff;
+    border-radius: 20px 20px 0 0;
+  }
+
+  &-body {
+    display: block;
+    background-color: #6fce96;
+    padding: 20px;
+    border-radius: 0 0 20px 20px;
+    font-weight: 500;
+    color: #fff;
+    font-size: 16px;
+
+    &::first-letter {
+      text-transform: capitalize;
+    }
   }
 }
 </style>
