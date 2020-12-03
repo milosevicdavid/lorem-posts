e34be3d4efe0dad8c41bca0329486e18f355db8e
David <milosevicdavid@gmail.com>
2020-12-03 12:07:13 +0100

    Card component separated

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="wrapper">
 
-      <v-select
+<v-select
         class="style-chooser"
         v-model="query"
         label="title"
@@ -29,18 +29,9 @@
           </template>
         </template>
       </v-select>
+      
+<Card :query="query" />      
 
-      <div v-if="query" class="card">
-        <div class="card-wrapper">
-          <span class="card-title"
-            ><h1>{{ query.title }}</h1></span
-          >
-          <span class="card-body">{{ `${query.body}.` }}</span>
-        </div>
-      </div>
-      <div class="post-holder" v-else>
-        ... your selected post will display here
-      </div>
     </div>
   </div>
 </template>
@@ -49,20 +40,24 @@
 
 <script>
 import axios from "axios";
+import Card from "./components/Card.vue"
 import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 
 export default {
   data() {
     return {
-      query: "",
       options: [],
+      query: ""
     };
   },
 
+
   components: {
-    PulseLoader,
+    Card,
+    PulseLoader
   },
 
+ 
   mounted: function() {
     return axios
       .get("https://jsonplaceholder.typicode.com/posts?q=lorem")
@@ -93,13 +88,6 @@ body {
 
 .wrapper {
   width: 65vw;
-}
-
-.post-holder {
-  color: #69c28e;
-  text-align: center;
-  margin-top: 100px;
-  font-size: 24px;
 }
 
 .style-chooser {
@@ -159,37 +147,5 @@ body {
   border-bottom: 1px solid #69c28e;
 }
 
-.card {
-  display: flex;
-  justify-content: center;
-  align-items: center;
 
-  &-wrapper {
-    width: 50vw;
-    margin-top: 50px;
-  }
-
-  &-title {
-    display: block;
-    text-transform: capitalize;
-    background-color: #69c28e;
-    padding: 20px;
-    color: #fff;
-    border-radius: 20px 20px 0 0;
-  }
-
-  &-body {
-    display: block;
-    background-color: #6fce96;
-    padding: 20px;
-    border-radius: 0 0 20px 20px;
-    font-weight: 500;
-    color: #fff;
-    font-size: 16px;
-
-    &::first-letter {
-      text-transform: capitalize;
-    }
-  }
-}
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,67 @@
+
+<template>
+<div v-if="query" class="card">
+        <div class="card-wrapper">
+          <span class="card-title"
+            ><h1>{{ query.title }}</h1></span
+          >
+          <span class="card-body">{{ `${query.body}.` }}</span>
+        </div>
+      </div>
+      <div class="post-holder" v-else>
+        ... your selected post will display here
+      </div>
+</template>
+
+<script>
+export default {
+
+  props: ['query']
+}
+
+</script>
+
+<style lang="scss">
+.post-holder {
+  color: #69c28e;
+  text-align: center;
+  margin-top: 100px;
+  font-size: 24px;
+}
+
+
+.card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &-wrapper {
+    width: 50vw;
+    margin-top: 50px;
+  }
+
+  &-title {
+    display: block;
+    text-transform: capitalize;
+    background-color: #69c28e;
+    padding: 20px;
+    color: #fff;
+    border-radius: 20px 20px 0 0;
+  }
+
+  &-body {
+    display: block;
+    background-color: #6fce96;
+    padding: 20px;
+    border-radius: 0 0 20px 20px;
+    font-weight: 500;
+    color: #fff;
+    font-size: 16px;
+
+    &::first-letter {
+      text-transform: capitalize;
+    }
+  }
+}
+
+</style>
